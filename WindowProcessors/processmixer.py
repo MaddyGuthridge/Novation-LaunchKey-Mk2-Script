0540525a73734992332c5f9443deac6000b2689b
HDSQ <31365175+MiguelGuthridge@users.noreply.github.com>
2020-07-15 18:55:46 +1000

    Arm recording only toggled when shift active

--- WindowProcessors/processmixer.py
+++ WindowProcessors/processmixer.py
@@ -25,160 +25,182 @@ def process(command):
     # Faders
     #---------------------------------
     if command.type == eventconsts.TYPE_FADER:
-        # Fader 1
-        if command.id == eventconsts.FADER_1: 
-            setVolume(command, 1, command.value)
-            command.handled = True
-        
-        # Fader 2
-        if command.id == eventconsts.FADER_2: 
-            setVolume(command, 2, command.value)
-            command.handled = True
-
-        # Fader 3
-        if command.id == eventconsts.FADER_3: 
-            setVolume(command, 3, command.value)
-            command.handled = True
-        
-        # Fader 4
-        if command.id == eventconsts.FADER_4: 
-            setVolume(command, 4, command.value)
-            command.handled = True
-
-        # Fader 5
-        if command.id == eventconsts.FADER_5: 
-            setVolume(command, 5, command.value)
-            command.handled = True
-        
-        # Fader 6
-        if command.id == eventconsts.FADER_6: 
-            setVolume(command, 6, command.value)
-            command.handled = True
-        
-        # Fader 7
-        if command.id == eventconsts.FADER_7: 
-            setVolume(command, 7, command.value)
-            command.handled = True
-
-        # Fader 8
-        if command.id == eventconsts.FADER_8: 
-            setVolume(command, 8, command.value)
-            command.handled = True
-
-        # Fader 9
-        if command.id == eventconsts.FADER_9: 
-            # If shift key held, change master track
-            if command.shifted:
-                setVolume(command, 0, command.value)
-                command.handled = True
-            # Otherwise change current track
-            else:
-                setVolume(command, current_track, command.value)
-                command.handled = True
+        processFaders(command)
 
     #---------------------------------
     # Knobs
     #---------------------------------
     if command.type == eventconsts.TYPE_KNOB:
-        # Knob 1
-        if command.id == eventconsts.KNOB_1: 
-            setPan(command, 1, command.value)
-            command.handled = True
-        
-        # Knob 2
-        if command.id == eventconsts.KNOB_2: 
-            setPan(command, 2, command.value)
-            command.handled = True
-
-        # Knob 3
-        if command.id == eventconsts.KNOB_3: 
-            setPan(command, 3, command.value)
-            command.handled = True
-        
-        # Knob 4
-        if command.id == eventconsts.KNOB_4: 
-            setPan(command, 4, command.value)
-            command.handled = True
-
-        # Knob 5
-        if command.id == eventconsts.KNOB_5: 
-            setPan(command, 5, command.value)
-            command.handled = True
-        
-        # Knob 6
-        if command.id == eventconsts.KNOB_6: 
-            setPan(command, 6, command.value)
-            command.handled = True
-        
-        # Knob 7
-        if command.id == eventconsts.KNOB_7: 
-            setPan(command, 7, command.value)
-            command.handled = True
-
-        # Knob 8 # Like fader 9: applies to current track or master
-        if command.id == eventconsts.KNOB_8: 
-            # If shift key held, change master track
-            if command.shifted:
-                setPan(command, 0, command.value)
-                command.handled = True
-            # Otherwise change current track
-            else:
-                setPan(command, current_track, command.value)
-                command.handled = True
+        processKnobs(command)
 
 
     #---------------------------------
     # Mixer Buttons - mute/solo tracks
     #---------------------------------
     if command.type == eventconsts.TYPE_FADER_BUTTON:
-        if command.id == eventconsts.FADER_BUTTON_1:
-            processMuteSolo(1, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_2:
-            processMuteSolo(2, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_3:
-            processMuteSolo(3, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_4:
-            processMuteSolo(4, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_5:
-            processMuteSolo(5, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_6:
-            processMuteSolo(6, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_7:
-            processMuteSolo(7, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_8:
-            processMuteSolo(8, command)
-            command.handled = True
-
-        if command.id == eventconsts.FADER_BUTTON_9:
-            # If shift key held, change master track
-            if command.shifted:
-                processMuteSolo(0, command)
-                command.handled = True
-            else:
-                processMuteSolo(current_track, command)
-                command.handled = True
+        processFaderButtons(command)
 
     #---------------------------------
     # Other
     #---------------------------------
 
-    if command.id == eventconsts.TRANSPORT_RECORD and command.is_lift:
+    # Arms current mixer track when shifted
+    if command.id == eventconsts.TRANSPORT_RECORD and command.is_lift and command.shifted:
         mixer.armTrack(current_track)
+        command.handle("Arm current mixer track")
+
+# Process fader events
+def processFaders(command):
+    current_track = mixer.trackNumber()
+
+
+    # Fader 1
+    if command.id == eventconsts.FADER_1: 
+        setVolume(command, 1, command.value)
         command.handled = True
+    
+    # Fader 2
+    if command.id == eventconsts.FADER_2: 
+        setVolume(command, 2, command.value)
+        command.handled = True
+
+    # Fader 3
+    if command.id == eventconsts.FADER_3: 
+        setVolume(command, 3, command.value)
+        command.handled = True
+    
+    # Fader 4
+    if command.id == eventconsts.FADER_4: 
+        setVolume(command, 4, command.value)
+        command.handled = True
+
+    # Fader 5
+    if command.id == eventconsts.FADER_5: 
+        setVolume(command, 5, command.value)
+        command.handled = True
+    
+    # Fader 6
+    if command.id == eventconsts.FADER_6: 
+        setVolume(command, 6, command.value)
+        command.handled = True
+    
+    # Fader 7
+    if command.id == eventconsts.FADER_7: 
+        setVolume(command, 7, command.value)
+        command.handled = True
+
+    # Fader 8
+    if command.id == eventconsts.FADER_8: 
+        setVolume(command, 8, command.value)
+        command.handled = True
+
+    # Fader 9
+    if command.id == eventconsts.FADER_9: 
+        # If shift key held, change master track
+        if command.shifted:
+            setVolume(command, 0, command.value)
+            command.handled = True
+        # Otherwise change current track
+        else:
+            setVolume(command, current_track, command.value)
+            command.handled = True
+
+# Process knob events
+def processKnobs(command):
+    current_track = mixer.trackNumber()
+
+
+    # Knob 1
+    if command.id == eventconsts.KNOB_1: 
+        setPan(command, 1, command.value)
+        command.handled = True
+    
+    # Knob 2
+    if command.id == eventconsts.KNOB_2: 
+        setPan(command, 2, command.value)
+        command.handled = True
+
+    # Knob 3
+    if command.id == eventconsts.KNOB_3: 
+        setPan(command, 3, command.value)
+        command.handled = True
+    
+    # Knob 4
+    if command.id == eventconsts.KNOB_4: 
+        setPan(command, 4, command.value)
+        command.handled = True
+
+    # Knob 5
+    if command.id == eventconsts.KNOB_5: 
+        setPan(command, 5, command.value)
+        command.handled = True
+    
+    # Knob 6
+    if command.id == eventconsts.KNOB_6: 
+        setPan(command, 6, command.value)
+        command.handled = True
+    
+    # Knob 7
+    if command.id == eventconsts.KNOB_7: 
+        setPan(command, 7, command.value)
+        command.handled = True
+
+    # Knob 8 # Like fader 9: applies to current track or master
+    if command.id == eventconsts.KNOB_8: 
+        # If shift key held, change master track
+        if command.shifted:
+            setPan(command, 0, command.value)
+            command.handled = True
+        # Otherwise change current track
+        else:
+            setPan(command, current_track, command.value)
+            command.handled = True
+
+# Process fader button events
+def processFaderButtons(command):
+    current_track = mixer.trackNumber()
+    
+    if command.id == eventconsts.FADER_BUTTON_1:
+        processMuteSolo(1, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_2:
+        processMuteSolo(2, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_3:
+        processMuteSolo(3, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_4:
+        processMuteSolo(4, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_5:
+        processMuteSolo(5, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_6:
+        processMuteSolo(6, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_7:
+        processMuteSolo(7, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_8:
+        processMuteSolo(8, command)
+        command.handled = True
+
+    if command.id == eventconsts.FADER_BUTTON_9:
+        # If shift key held, change master track
+        if command.shifted:
+            processMuteSolo(0, command)
+            command.handled = True
+        else:
+            processMuteSolo(current_track, command)
+            command.handled = True
+
 
 def redraw(lights):
 
